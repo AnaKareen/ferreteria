27eb90f76023aa16b7dfe5932a8b40c844507f19
Gustavo Ramírez Mireles <gusramireles@hotmail.com>
2024-03-04 21:21:18 -0600

    Carga de la imagen a la carpeta uploads\productos"

--- admin/sistema.class.php
+++ admin/sistema.class.php
@@ -12,11 +12,25 @@ class Sistema
         $this->conn = new PDO("mysql:host=$servername;dbname=$mydb", $username, $password);
     }
 
-    function setCount($count) {
+    function setCount($count)
+    {
         $this->count = $count;
     }
 
-    function getCount() {
+    function getCount()
+    {
         return $this->count;
+    }
+
+    function upload()
+    {
+        $permitidos = array('image/jpeg', 'image/png');
+        if (in_array($_FILES['fotografia']['type'], $permitidos)) {
+            if ($_FILES['fotografia']['size'] <= 1024000) {
+                move_uploaded_file($_FILES['fotografia']['tmp_name'], '..\\uploads\\productos\\'.$_FILES['fotografia']['name']);
+                return $_FILES['fotografia']['name'];
+            }
+        }
+        return false;
     }
 }
